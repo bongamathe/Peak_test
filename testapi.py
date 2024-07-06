68fb501dcb8498a4a17ffb5539b65c4cf7e2883b
Bonga Mathe <bonga706@gmail.com>
2024-07-06 12:54:50 +0200

    looked at alphavantage api

--- testapi.py
+++ testapi.py
@@ -1,13 +1,19 @@
 import requests
 
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=J40JYFDGGPQBLB3W'
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=J40JYFDGGPQBLB3W'
 r = requests.get(url)
 data = r.json()
 
 print(data['Meta Data'])
 print("=========================================================================================================================")
-print(data['Time Series (5min)'])
-print("=========================================================================================================================")
-print(data)
+key_date = None
+for index, date in enumerate(data['Time Series (Daily)'].keys(), start=1):
+    print(f"{index} : {date}")
+    if index == 1:
+        key_date = date
+print("==========================================================================================================================")
+print("Type of data from daily dictionary: ")
+print(f"Date : {key_date}")
+print(data['Time Series (Daily)'][key_date])
 print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
 print("Learning git")
